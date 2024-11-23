1b0d00f1603d444f3ad36076be2f055a8fafe585
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-11-22 20:33:23 -0500

    bump

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # auth in .pypirc
 (cd external/rl-tools/static/ui_server/generic && ./download_dependencies.sh)
-rm -rf ../python-interface/dist
+rm -rf ../l2f/dist
 pip install --upgrade build twine
 python3 -m build --sdist
 python3 -m twine upload dist/*
