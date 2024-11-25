ad49fec81fd49d10ba870cd0e3d4501ddb90d3a1
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-11-25 12:04:24 -0500

    splitting ppo and sac

--- examples/sb3_sac.py
+++ examples/sb3_sac.py
@@ -1,25 +1,23 @@
 from l2f.wrappers.sb3 import L2F
-from stable_baselines3 import SAC, PPO
+from stable_baselines3 import SAC
 from stable_baselines3.common.vec_env import VecMonitor
 import time
 import torch
 
 
 
-n_envs = 64
+n_envs = 16
 vec_env = L2F(n_envs)
 vec_env = VecMonitor(vec_env)
 
-hidden_dim = 64
+hidden_dim = 32
 policy_kwargs = dict(
     activation_fn=torch.nn.ReLU,
     net_arch=dict(pi=[hidden_dim, hidden_dim], vf=[hidden_dim, hidden_dim], qf=[hidden_dim, hidden_dim]),
 )
 
-N = 40000000
-model = PPO("MlpPolicy", vec_env, verbose=1, n_epochs=1, n_steps=128,batch_size = 4096, policy_kwargs=policy_kwargs, normalize_advantage=True, learning_rate=1e-3, vf_coef=1, tensorboard_log="./tensorboard/")
-# N = 400000
-# model = SAC("MlpPolicy", vec_env, buffer_size=N, verbose=1, tensorboard_log="./tensorboard/")
+N = 4000000
+model = SAC("MlpPolicy", vec_env, buffer_size=N, verbose=1, tensorboard_log="./tensorboard/", gradient_steps=1) #, train_freq=10)
 model.learn(total_timesteps=N)
 
 while True:
