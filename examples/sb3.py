4046fd2b94c216140c9f0fa0e101e7fde61f4b83
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-11-25 13:49:10 -0500

    td3 also good

--- examples/sb3.py
+++ examples/sb3.py
@@ -5,13 +5,13 @@ import time
 import torch
 
 library = "sbx"
-algorithm = "sac"
+algorithm = "td3"
 
 
 if library == "sbx":
-    from sbx import PPO, SAC
+    from sbx import PPO, SAC, TD3
 elif library == "sb3":
-    from stable_baselines3 import PPO, SAC
+    from stable_baselines3 import PPO, SAC, TD3
 
 if algorithm == "ppo":
     n_envs = 64
@@ -27,7 +27,7 @@ if algorithm == "ppo":
     N = 40000000
     model = PPO("MlpPolicy", vec_env, verbose=1, n_epochs=1, n_steps=128,batch_size = 4096, policy_kwargs=policy_kwargs, normalize_advantage=True, learning_rate=1e-3, vf_coef=1, tensorboard_log="./tensorboard/")
     model.learn(total_timesteps=N)
-else:
+elif algorithm == "sac":
     n_envs = 16
     vec_env = L2F(n_envs)
     vec_env = VecMonitor(vec_env)
@@ -41,6 +41,22 @@ else:
     N = 4000000
     model = SAC("MlpPolicy", vec_env, buffer_size=N, verbose=1, tensorboard_log="./tensorboard/", gradient_steps=1) #, train_freq=10)
     model.learn(total_timesteps=N)
+elif algorithm == "td3":
+    n_envs = 16
+    vec_env = L2F(n_envs)
+    vec_env = VecMonitor(vec_env)
+
+    hidden_dim = 32
+    policy_kwargs = dict(
+        activation_fn=torch.nn.ReLU,
+        net_arch=dict(pi=[hidden_dim, hidden_dim], vf=[hidden_dim, hidden_dim], qf=[hidden_dim, hidden_dim]),
+    )
+
+    N = 4000000
+    model = TD3("MlpPolicy", vec_env, buffer_size=N, verbose=1, tensorboard_log="./tensorboard/", gradient_steps=1) #, train_freq=10)
+    model.learn(total_timesteps=N)
+else:
+    raise ValueError(f"Unknown algorithm {algorithm}")
 
 
 obs = vec_env.reset()
